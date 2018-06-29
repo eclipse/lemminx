c2d26706038378702cfe3d434bb5b4bd7392eaa2
angelozerr <angelo.zerr@gmail.com>
2018-06-29 06:42:16 +0200

    Add missings tags test for XML parser (see #7)

--- org.eclipse.xml.languageserver/src/test/java/org/eclipse/xml/languageserver/internal/parser/XMLParserTest.java
+++ org.eclipse.xml.languageserver/src/test/java/org/eclipse/xml/languageserver/internal/parser/XMLParserTest.java
@@ -16,13 +16,39 @@ public class XMLParserTest {
 				"[{ tag: 'html', start: 0, end: 13, endTagStart: 6, closed: true, children: [] }]");
 		assertDocument("<html><body></body></html>",
 				"[{ tag: 'html', start: 0, end: 26, endTagStart: 19, closed: true, children: [{ tag: 'body', start: 6, end: 19, endTagStart: 12, closed: true, children: [] }] }]");
-		//assertDocument("<html><head></head><body></body></html>",
-		//		"[{ tag: 'html', start: 0, end: 39, endTagStart: 32, closed: true, children: [{ tag: 'head', start: 6, end: 19, endTagStart: 12, closed: true, children: [] }, { tag: 'body', start: 19, end: 32, endTagStart: 25, closed: true, children: [] }] }]");
+		assertDocument("<html><head></head><body></body></html>",
+				"[{ tag: 'html', start: 0, end: 39, endTagStart: 32, closed: true, children: [{ tag: 'head', start: 6, end: 19, endTagStart: 12, closed: true, children: [] }, { tag: 'body', start: 19, end: 32, endTagStart: 25, closed: true, children: [] }] }]");
+	}
+
+	@Test
+	public void testSelfClose() {
+		assertDocument("<br/>", "[{ tag: 'br', start: 0, end: 5, endTagStart: null, closed: true, children: [] }]");
+		assertDocument("<div><br/><span></span></div>",
+				"[{ tag: 'div', start: 0, end: 29, endTagStart: 23, closed: true, children: [{ tag: 'br', start: 5, end: 10, endTagStart: null, closed: true, children: [] }, { tag: 'span', start: 10, end: 23, endTagStart: 16, closed: true, children: [] }] }]");
+	}
+
+	@Test
+	public void testEmptyTag() {
+		assertDocument("<meta>", "[{ tag: 'meta', start: 0, end: 6, endTagStart: null, closed: false, children: [] }]");
+		/*assertDocument("<div><input type=\"button\" ><span><br><br></span></div>", "[{ "
+				+ "tag: 'div', start: 0, end: 53, endTagStart: 47, closed: true, children: ["
+				+ "{ tag: 'input', start: 5, end: 26, endTagStart: null, closed: true, children: [] },"
+				+ "{ tag: 'span', start: 26, end: 47, endTagStart: 40, closed: true, children: [{ tag: 'br', start: 32, end: 36, endTagStart: null, closed: true, children: [] }, { tag: 'br', start: 36, end: 40, endTagStart: null, closed: true, children: [] }] }"
+				+ "]" + "}]");*/
+	}
+	
+	@Test
+	public void testMissingTags()  {
+		assertDocument("</meta>", "[]");
+		assertDocument("<div></div></div>", "[{ tag: 'div', start: 0, end: 11, endTagStart: 5, closed: true, children: [] }]");
+		assertDocument("<div><div></div>", "[{ tag: 'div', start: 0, end: 16, endTagStart: null, closed: false, children: [{ tag: 'div', start: 5, end: 16, endTagStart: 10, closed: true, children: [] }] }]");
+		assertDocument("<title><div></title>", "[{ tag: 'title', start: 0, end: 20, endTagStart: 12, closed: true, children: [{ tag: 'div', start: 7, end: 12, endTagStart: null, closed: false, children: [] }] }]");
+		assertDocument("<h1><div><span></h1>", "[{ tag: 'h1', start: 0, end: 20, endTagStart: 15, closed: true, children: [{ tag: 'div', start: 4, end: 15, endTagStart: null, closed: false, children: [{ tag: 'span', start: 9, end: 15, endTagStart: null, closed: false, children: [] }] }] }]");
 	}
 
 	private static void assertDocument(String input, String expected) {
 		XMLDocument document = XMLParser.getInstance().parse(input);
-		Assert.assertEquals(toJSON(document.getRoots()), expected);
+		Assert.assertEquals(expected, toJSON(document.getRoots()));
 	}
 
 	private static String toJSON(Node node) {
@@ -31,7 +57,7 @@ public class XMLParserTest {
 	}
 
 	private static String toJSON(List<Node> nodes) {
-		return "[" + nodes.stream().map(XMLParserTest::toJSON).collect(Collectors.joining(",")) + "]";
+		return "[" + nodes.stream().map(XMLParserTest::toJSON).collect(Collectors.joining(", ")) + "]";
 	}
 
 }
