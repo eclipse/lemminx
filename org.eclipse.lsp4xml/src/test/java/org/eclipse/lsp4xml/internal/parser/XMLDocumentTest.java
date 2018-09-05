e092e44d5a716270fbee707353ab0f66fc01c9f6
angelozerr <angelo.zerr@gmail.com>
2018-09-05 12:14:43 +0200

    Add XMLDocumentTest to test performance (see #48)

---
+++ org.eclipse.lsp4xml/src/test/java/org/eclipse/lsp4xml/internal/parser/XMLDocumentTest.java
@@ -0,0 +1,25 @@
+package org.eclipse.lsp4xml.internal.parser;
+
+import java.io.InputStream;
+
+import org.eclipse.lsp4xml.commons.TextDocument;
+import org.eclipse.lsp4xml.model.XMLDocument;
+import org.junit.Test;
+
+public class XMLDocumentTest {
+
+	@Test
+	public void testLargeFile() {
+		InputStream in = XMLDocumentTest.class.getResourceAsStream("/xml/largeFile.xml");
+		String text = convertStreamToString(in);
+		TextDocument document = new TextDocument(text, "largeFile.xml");
+		long start = System.currentTimeMillis();
+		XMLDocument xmlDocument = XMLParser.getInstance().parse(document);
+		System.err.println("Parsed in " + (System.currentTimeMillis() - start) + " ms.");
+	}
+
+	static String convertStreamToString(InputStream is) {
+		java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+		return s.hasNext() ? s.next() : "";
+	}
+}
