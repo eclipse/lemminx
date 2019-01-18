006f6306c9546c671f4fbdd68351339bc0a33eb4
Fred Bricon <fbricon@gmail.com>
2019-01-17 22:11:08 -0500

    Setup Maven deployment - part 5
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ echo "Setting bogus version"
 ./mvnw versions:set -DnewVersion=0.0.0
 
 echo "Deploying to Maven repo"
-./mvnw --settings travis-settings.xml deploy
+./mvnw --settings travis-settings.xml deploy -DskipTests
