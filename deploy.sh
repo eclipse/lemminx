30af7580aaa390984106ae37e9f2a6bad81fbda5
Fred Bricon <fbricon@gmail.com>
2019-01-28 09:26:13 -0500

    Perform a release deployment when tag is set to a version
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-#echo "Setting release version"
-#./mvnw versions:set -DnewVersion=$TRAVIS_TAG
+if [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then 
+  echo "Setting release version to tag $TRAVIS_TAG"
+  ./mvnw versions:set -DnewVersion=$TRAVIS_TAG
+fi
 
 echo "Deploying to Maven repo"
 ./mvnw --settings travis-settings.xml deploy -DskipTests -Pci
