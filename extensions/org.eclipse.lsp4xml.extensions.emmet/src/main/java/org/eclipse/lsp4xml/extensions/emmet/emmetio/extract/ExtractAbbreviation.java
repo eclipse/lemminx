2bb8f661475a87507f58329b0b5754a46078b74f
Fred Bricon <fbricon@gmail.com>
2019-01-28 10:54:49 -0500

    Fix license headers according to project's declared EPL v2.0
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- extensions/org.eclipse.lsp4xml.extensions.emmet/src/main/java/org/eclipse/lsp4xml/extensions/emmet/emmetio/extract/ExtractAbbreviation.java
+++ extensions/org.eclipse.lsp4xml.extensions.emmet/src/main/java/org/eclipse/lsp4xml/extensions/emmet/emmetio/extract/ExtractAbbreviation.java
@@ -1,3 +1,13 @@
+/**
+ *  Copyright (c) 2018 Angelo ZERR
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v2.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v20.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
 package org.eclipse.lsp4xml.extensions.emmet.emmetio.extract;
 
 import java.io.InputStreamReader;
--- org.eclipse.lsp4xml/src/main/java/org/eclipse/lsp4xml/commons/Line.java
+++ org.eclipse.lsp4xml/src/main/java/org/eclipse/lsp4xml/commons/Line.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-v20.html
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
