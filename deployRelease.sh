99eb906bbf5aa16778b81cbc779943eeb467f65b
Fred Bricon <fbricon@gmail.com>
2019-03-05 16:19:49 -0500

    Artifactory deployment, take 2
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- deployRelease.sh
+++ deployRelease.sh
@@ -8,7 +8,7 @@ then
   echo "Deployed to Maven repository."
   echo "Now uploading the plug-ins to p2 repo on Bintray..."
   BINTRAY_URL=https://api.bintray.com/content/lsp4xml/p2/lsp4xml
-  curl -T org.eclipse.lsp4xml/target/org.eclipse.lsp4xml-p2repo.zip	-u ${RELEASES_USERNAME}:${RELEASES_PASSWORD} -H "X-Bintray-Version:${VERSION}" -H "X-Bintray-Explode:1"  ${BINTRAY_URL}/${TRAVIS_TAG}/
+  curl -T org.eclipse.lsp4xml/target/org.eclipse.lsp4xml-p2repo.zip	-u $RELEASES_USERNAME:$RELEASES_PASSWORD -H "X-Bintray-Version:$VERSION" -H "X-Bintray-Explode:1"  $BINTRAY_URL/$TRAVIS_TAG/
 else
   echo "Could not deploy to Maven repository"
 fi
--- deploySnapshot.sh
+++ deploySnapshot.sh
@@ -7,7 +7,7 @@ then
   echo "Deployed to Maven repository."
   echo "Now uploading the plug-ins to the p2 repo on Artifactory..."
   BINTRAY_URL=https://api.bintray.com/content/lsp4xml/p2/lsp4xml
-  curl -H 'X-JFrog-Art-Api:${ARTIFACTORY_TOKEN}' -H 'X-Explode-Archive-Atomic: true' -H 'X-Explode-Archive: true' https://lsp4xml.jfrog.io/lsp4xml/generic-local/p2/lsp4xml/${VERSION}  --upload-file org.eclipse.lsp4xml/target/org.eclipse.lsp4xml-p2repo.zip
+  curl -H "X-JFrog-Art-Api:$ARTIFACTORY_TOKEN" -H 'X-Explode-Archive-Atomic: true' -H 'X-Explode-Archive: true' https://lsp4xml.jfrog.io/lsp4xml/generic-local/p2/lsp4xml/$VERSION  --upload-file org.eclipse.lsp4xml/target/org.eclipse.lsp4xml-p2repo.zip
 else
   echo "Could not deploy to Maven repository"
 fi
