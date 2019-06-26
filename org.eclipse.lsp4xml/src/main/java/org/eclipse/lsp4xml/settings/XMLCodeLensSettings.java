029bafd6a4d49ecd2e6b65755d903a8bcd0cadb7
Angelo <angelo.zerr@gmail.com>
2019-06-26 22:13:20 +0200

    Disable XML Codelens by default
    Disable XML Codelens by default

--- org.eclipse.lsp4xml/src/main/java/org/eclipse/lsp4xml/settings/XMLCodeLensSettings.java
+++ org.eclipse.lsp4xml/src/main/java/org/eclipse/lsp4xml/settings/XMLCodeLensSettings.java
@@ -1,8 +1,23 @@
+/*******************************************************************************
+* Copyright (c) 2019 Red Hat Inc. and others.
+* All rights reserved. This program and the accompanying materials
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v20.html
+*
+* Contributors:
+*     Red Hat Inc. - initial API and implementation
+*******************************************************************************/
 package org.eclipse.lsp4xml.settings;
 
+/**
+ * XML CodeLens settings
+ * 
+ * @author Angelo ZERR
+ *
+ */
 public class XMLCodeLensSettings {
 
-	private boolean enabled = true;
+	private boolean enabled = false;
 
 	public boolean isEnabled() {
 		return enabled;
