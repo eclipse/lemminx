67b93acb9fa7bd175bc49d6a8f9d62c56bf88700
azerr <azerr@redhat.com>
2019-07-17 12:40:20 +0200

    Documents with an Internal Subset DOCTYPE shouldn't stop trying to bind
    Fix #379
    
    Signed-off-by: azerr <azerr@redhat.com>

---
+++ org.eclipse.lsp4xml/src/main/java/org/eclipse/lsp4xml/extensions/contentmodel/participants/diagnostics/LSPXMLParserConfiguration.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+* Copyright (c) 2019 Red Hat Inc. and others.
+* All rights reserved. This program and the accompanying materials
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v20.html
+*
+* Contributors:
+*     Red Hat Inc. - initial API and implementation
+*******************************************************************************/
+package org.eclipse.lsp4xml.extensions.contentmodel.participants.diagnostics;
+
+import org.apache.xerces.impl.dtd.XMLDTDValidator;
+import org.apache.xerces.parsers.XIncludeAwareParserConfiguration;
+import org.apache.xerces.xni.XNIException;
+import org.apache.xerces.xni.parser.XMLComponentManager;
+import org.apache.xerces.xni.parser.XMLConfigurationException;
+
+/**
+ * Custom Xerces XML parser configuration to :
+ * 
+ * <ul>
+ * <li>disable only DTD validation if required</li>
+ * </ul>
+ *
+ */
+class LSPXMLParserConfiguration extends XIncludeAwareParserConfiguration {
+
+	private final boolean disableDTDValidation;
+
+	public LSPXMLParserConfiguration(boolean disableDTDValidation) {
+		this.disableDTDValidation = disableDTDValidation;
+	}
+
+	@Override
+	protected void reset() throws XNIException {
+		super.reset();
+		if (disableDTDValidation) {
+			// reset again DTD validator by setting "http://xml.org/sax/features/validation"
+			// to false.
+			disableDTDValidation();
+		}
+	}
+
+	private void disableDTDValidation() {
+		XMLDTDValidator validator = (XMLDTDValidator) super.getProperty(DTD_VALIDATOR);
+		if (validator != null) {
+			// Calling XMLDTDValidator#setFeature("http://xml.org/sax/features/validation",
+			// false) does nothing.
+			// The only way to set "http://xml.org/sax/features/validation" to false is to
+			// call XMLDTDValidator#reset with the proper component.
+			// We need to create a new component and not use the current configuration
+			// otherwise set
+			// "http://xml.org/sax/features/validation" to the configuration
+			// will update the other component and will disable validation too for XML
+			// Schema
+			XMLComponentManager disableDTDComponent = new XMLComponentManager() {
+
+				@Override
+				public Object getProperty(String propertyId) throws XMLConfigurationException {
+					return LSPXMLParserConfiguration.this.getProperty(propertyId);
+				}
+
+				@Override
+				public boolean getFeature(String featureId) throws XMLConfigurationException {
+					if ("http://xml.org/sax/features/validation".equals(featureId)) {
+						return false;
+					}
+					return LSPXMLParserConfiguration.this.getFeature(featureId);
+				}
+			};
+			validator.reset(disableDTDComponent);
+		}
+	}
+
+}
