abb1a1ef26d6769fddf34a2989e759ca9c173470
blandolt <balduin.landolt@unibas.ch>
2020-04-19 08:18:29 +0200

    Feature: potential test case files

---
+++ org.eclipse.lemminx/src/test/java/org/eclipse/lemminx/extensions/pi/XMLModelTest.java
@@ -0,0 +1,19 @@
+package org.eclipse.lemminx.extensions.pi;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.eclipse.lemminx.XMLAssert;
+import org.eclipse.lemminx.XMLTextDocumentService;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+public class XMLModelTest {
+	@Test
+	public void xmlModelDeclaration(){
+		//XMLTextDocumentService service = new XMLTextDocumentService(xmlLanguageServer)
+		//TODO: check if document with xml-model has grammar
+		assertTrue(hasGrammar);
+	}
+}
