41bbb216c0056853ef08167cac2a57452574160f
blandolt <balduin.landolt@unibas.ch>
2020-05-10 14:36:52 +0200

    Feature: implement XMLModel

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/dom/XMLModel.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/dom/XMLModel.java
@@ -10,13 +10,67 @@
 
 package org.eclipse.lemminx.dom;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.w3c.dom.ProcessingInstruction;
+
 /**
  * XMLModel
  * 
  * xml-model processing instruction {@code <?xml-model ...>}
+ * 
+ * see https://www.w3.org/TR/xml-model/
  */
 public class XMLModel {
 
-	//TODO: implement!
+	
+	// possible attributes according to https://www.w3.org/TR/xml-model/
+	// but there may be more.
+	// LATER: what to do with things like e.g.: <?oxygen RNGSchema="http://..." type="xml" ?>
+	public static final String HREF = "href";
+	public static final String TYPE = "type";
+	public static final String SCHEMATYPENS = "schematypens";
+	public static final String CHARSET = "charset";
+	public static final String TITLE = "title";
+	public static final String GROUP = "group";
+	public static final String PHASE = "phase";
+
+	private Map<String, String> valuePairs;
+
+	/**
+	 * Creates an XMLModel object from a ProcessingInstruction object.
+	 * It is assumed that the processing instruction is of type {@code <?xml-model ?>}
+	 * 
+	 * @param pi xml-model Processing Instruction
+	 */
+	public XMLModel(ProcessingInstruction pi) {
+		valuePairs = new HashMap<>();
+		String data = pi.getData();
+		String[] tokens = data.split("\\s");
+		for (String token : tokens) {
+			putAttribute(token);
+		}
+	}
+
+	private void putAttribute(String token) {
+		String[] ss = token.split("=");
+		if (ss.length < 2)
+			return;
+		String key = ss[0].toLowerCase().trim();
+		String val = ss[1].replace("\"", "");
+		valuePairs.put(key, val);
+	}
+
+	public String getAttribute(String name){
+		return valuePairs.get(name);
+	}
+
+	public String getSchemaLocation(){
+		return getAttribute(HREF);
+	}
+
+	// TODO: add methods for other typicle attributes (see fields)
+
 }
 
