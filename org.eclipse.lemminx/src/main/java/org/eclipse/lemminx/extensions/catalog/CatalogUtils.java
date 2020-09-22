dbf43f3cae27a726c22e319eca939872c83fb3d8
David Thompson <davthomp@redhat.com>
2020-09-22 16:52:39 -0400

    Doclink for catalog entries with 'catalog' attr
    Any of the catalog entry elements that have the 'catalog' attribute
    to refer to another catalog are now doclinks.
    
    Use xml:base when making catalog doc links
    
    Doc links now work inside `<group></group>`.
    xml:base is taken into account when calculating where a doc link should
    point in catalog files.
    
    Closes #845
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/extensions/catalog/CatalogUtils.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/extensions/catalog/CatalogUtils.java
@@ -10,17 +10,19 @@
 
 package org.eclipse.lemminx.extensions.catalog;
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.eclipse.lemminx.dom.DOMAttr;
 import org.eclipse.lemminx.dom.DOMDocument;
 import org.eclipse.lemminx.dom.DOMElement;
 import org.eclipse.lemminx.dom.DOMNode;
 import org.eclipse.lemminx.utils.DOMUtils;
+import org.eclipse.lsp4j.jsonrpc.validation.NonNull;
 
 /**
  * Utility functions for working with XML catalog documents
@@ -30,30 +32,39 @@ public class CatalogUtils {
 	/**
 	 * The catalog entries that have a 'uri' attribute
 	 */
-	private static final Collection<String> CATALOG_NAMES = Arrays.asList("public", "system", "uri", "systemSuffix",
+	private static final Collection<String> HAS_URI_ATTRIBUTE = Arrays.asList("public", "system", "uri", "systemSuffix",
 			"uriSuffix");
+
+	/**
+	 * The catalog entries that have a 'catalog' attribute
+	 */
+	private static final Collection<String> HAS_CATALOG_ATTRIBUTE = Arrays.asList("delegatePublic", "delegateSystem",
+			"delegateUri", "nextCatalog");
+
+	private static final String XML_BASE_ATTRIBUTE = "xml:base";
 
 	private static final String CATALOG_ENTITY_NAME = "catalog";
 
+	private static final String GROUP_ENTITY_NAME = "group";
+
 	private static final String URI_ATTRIBUTE_NAME = "uri";
 
+	private static final String CATALOG_ATTRIBUTE_NAME = "catalog";
+
 	/**
-	 * Returns a list of all the catalog entries that have the attribute 'uri', or
-	 * an empty list if the document is not a catalog.
+	 * Returns a list of all the catalog entries in the given document, or an empty
+	 * list if the document is not a catalog.
 	 *
-	 * @param document The document to collect the catalog entries from
-	 * @return A list of all the catalog entries that have the attribute 'uri', or
-	 *         an empty list if the document is not a catalog.
+	 * @return a list of all the catalog entries in the given document, or an empty
+	 *         list if the document is not a catalog.
 	 */
-	public static List<DOMElement> getCatalogEntries(DOMDocument document) {
+	public static List<CatalogEntry> getCatalogEntries(DOMDocument document) {
 		if (!DOMUtils.isCatalog(document)) {
 			return Collections.emptyList();
 		}
 		for (DOMNode n : document.getChildren()) {
 			if (CATALOG_ENTITY_NAME.equals(n.getNodeName())) {
-				return n.getChildren().stream().filter(CatalogUtils::isCatalogURIEntry).map((el) -> {
-					return (DOMElement) el;
-				}).collect(Collectors.toList());
+				return collectCatalogEntries((DOMElement) n);
 			}
 		}
 		return Collections.emptyList();
@@ -72,15 +83,114 @@ public class CatalogUtils {
 	}
 
 	/**
-	 * Checks if this node is a catalog entry that is required to have the 'uri'
-	 * attribute
+	 * Returns the catalog attribute node of the given catalog entry or null if
+	 * there is no catalog attribute
 	 *
-	 * @param node The node to check
-	 * @return true if this node is an catalog entry that is required to have the
+	 * @param element The catalog entry to get the catalog attribute of
+	 * @return the catalog attribute node of the given catalog entry or null if
+	 *         there is no catalog attribute
+	 */
+	public static DOMAttr getCatalogEntryCatalog(DOMElement element) {
+		return element.getAttributeNode(CATALOG_ATTRIBUTE_NAME);
+	}
+
+	/**
+	 * Returns true if this element is a catalog entry that is required to have the
+	 * 'uri' attribute and false otherwise
+	 *
+	 * @param element The element to check
+	 * @return true if this element is an catalog entry that is required to have the
 	 *         'uri' attribute and false otherwise
 	 */
-	private static boolean isCatalogURIEntry(DOMNode node) {
-		return node.isElement() && CATALOG_NAMES.contains(node.getNodeName());
+	private static boolean isCatalogEntryWithURI(DOMElement element) {
+		return HAS_URI_ATTRIBUTE.contains(element.getNodeName());
+	}
+
+	/**
+	 * Returns true if this element is a catalog entry that is required to have the
+	 * 'catalog' attribute and false otherwise
+	 *
+	 * @param element The element to check
+	 * @return true if this element is an catalog entry that is required to have the
+	 *         'catalog' attribute and false otherwise
+	 */
+	private static boolean isCatalogEntryWithCatalog(DOMElement element) {
+		return HAS_CATALOG_ATTRIBUTE.contains(element.getNodeName());
+	}
+
+	/**
+	 * Returns true if the given element is a group element and false otherwise
+	 *
+	 * @param element The element to check
+	 * @return true if the given element is a group element and false otherwise
+	 */
+	private static boolean isGroupCatalogEntry(DOMElement element) {
+		return GROUP_ENTITY_NAME.equals(element.getNodeName());
+	}
+
+	/**
+	 * Get a list of all catalog entry elements that are in the given catalog
+	 *
+	 * @param catalog the catalog element to collect the entries for
+	 * @return A list of all the catalog entities in this catalog
+	 */
+	private static List<CatalogEntry> collectCatalogEntries(@NonNull DOMElement catalog) {
+		List<CatalogEntry> entries = new ArrayList<>();
+		String baseURI = catalog.getAttribute(XML_BASE_ATTRIBUTE);
+		baseURI = baseURI == null ? "" : baseURI;
+		for (DOMNode node : catalog.getChildren()) {
+			if (node.isElement()) {
+				DOMElement element = (DOMElement) node;
+				CatalogEntry catalogEntry = createCatalogEntry(baseURI, element);
+				if (catalogEntry != null) {
+					entries.add(catalogEntry);
+				} else if (isGroupCatalogEntry(element)) {
+					entries.addAll(collectGroupEntries(element, baseURI));
+				}
+			}
+		}
+		return entries;
+	}
+
+	/**
+	 * Get a list of all catalog entry elements that are in the given group
+	 *
+	 * @param group   the group element to collect the entries for
+	 * @param baseURI the baseURI of the catalog
+	 * @return A list of all the catalog entities in this group
+	 */
+	private static List<CatalogEntry> collectGroupEntries(DOMElement group, @NonNull String baseURI) {
+		List<CatalogEntry> entries = new ArrayList<>();
+		String groupSegment = group.getAttribute(XML_BASE_ATTRIBUTE);
+		if (groupSegment != null) {
+			baseURI = Paths.get(baseURI, groupSegment).toString();
+		}
+		for (DOMNode node : group.getChildren()) {
+			if (node.isElement()) {
+				DOMElement element = (DOMElement) node;
+				CatalogEntry catalogEntry = createCatalogEntry(baseURI, element);
+				if (catalogEntry != null) {
+					entries.add(catalogEntry);
+				}
+			}
+		}
+		return entries;
+	}
+
+	/**
+	 * Returns a catalog entry for the given element and null if a catalog entry can't be made
+	 *
+	 * @param baseURI the base URI of the catalog entry
+	 * @param element the element to turn into a catalog entry
+	 * @return a catalog entry for the given element and null if a catalog entry can't be made
+	 */
+	private static CatalogEntry createCatalogEntry(@NonNull String baseURI, DOMElement element) {
+		if (isCatalogEntryWithURI(element)) {
+			return new URICatalogEntry(baseURI, element);
+		} else if (isCatalogEntryWithCatalog(element)) {
+			return new CatalogCatalogEntry(baseURI, element);
+		}
+		return null;
 	}
 
 }
