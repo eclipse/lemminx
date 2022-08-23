5bfe06a35f4ac30b3dcb3134f8d8a155cb0e838a
Jessica He <jhe@redhat.com>
2022-08-23 14:53:32 -0400

    Support spaceBeforeEmptyCloseTag setting with experimental formatter
    Signed-off-by: Jessica He <jhe@redhat.com>

--- org.eclipse.lemminx/src/test/java/org/eclipse/lemminx/services/format/experimental/XMLFormatterSetSpaceBeforeEmptyCloseTagTest.java
+++ org.eclipse.lemminx/src/test/java/org/eclipse/lemminx/services/format/experimental/XMLFormatterSetSpaceBeforeEmptyCloseTagTest.java
@@ -17,28 +17,67 @@ import org.eclipse.lemminx.XMLAssert;
 import org.eclipse.lemminx.commons.BadLocationException;
 import org.eclipse.lemminx.settings.SharedSettings;
 import org.eclipse.lsp4j.TextEdit;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
- * XML experimental formatter services tests with setSpaceBeforeEmptyCloseTag setting.
+ * XML experimental formatter services tests with setSpaceBeforeEmptyCloseTag
+ * setting.
  *
  */
 public class XMLFormatterSetSpaceBeforeEmptyCloseTagTest {
 
-	@Disabled
+	@Test
+	public void testSelfCloseTagSpaceSingleElement() throws BadLocationException {
+		SharedSettings settings = new SharedSettings();
+		settings.getFormattingSettings().setSpaceBeforeEmptyCloseTag(true);
+
+		String content = "<a/>";
+		String expected = "<a />";
+		assertFormat(content, expected, settings,
+				te(0, 2, 0, 2, " "));
+		assertFormat(expected, expected, settings);
+	}
+
+	@Test
+	public void testSelfCloseTagSpaceSingleElementMultipleSpaces() throws BadLocationException {
+		SharedSettings settings = new SharedSettings();
+		settings.getFormattingSettings().setSpaceBeforeEmptyCloseTag(true);
+
+		String content = "<a         />";
+		String expected = "<a />";
+		assertFormat(content, expected, settings,
+				te(0, 2, 0, 11, " "));
+		assertFormat(expected, expected, settings);
+	}
+
+	@Test
+	public void testSelfCloseTagSpaceSingleElementNewLine() throws BadLocationException {
+		SharedSettings settings = new SharedSettings();
+		settings.getFormattingSettings().setSpaceBeforeEmptyCloseTag(true);
+
+		String content = "<a\r\n" + //
+				"  />";
+		String expected = "<a />";
+		assertFormat(content, expected, settings,
+				te(0, 2, 1, 2, " "));
+		assertFormat(expected, expected, settings);
+	}
+
 	@Test
 	public void testSelfCloseTagSpace() throws BadLocationException {
 		SharedSettings settings = new SharedSettings();
 		settings.getFormattingSettings().setSpaceBeforeEmptyCloseTag(true);
 
-		String content = "<a>\r" + //
-				" <b/>\r" + //
+		String content = "<a>\r\n" + //
+				" <b/>\r\n" + //
 				"</a>";
-		String expected = "<a>\r" + //
-				"  <b />\r" + //
+		String expected = "<a>\r\n" + //
+				"  <b />\r\n" + //
 				"</a>";
-		assertFormat(content, expected, settings);
+		assertFormat(content, expected, settings,
+				te(0, 3, 1, 1, "\r\n  "),
+				te(1, 3, 1, 3, " "));
+		assertFormat(expected, expected, settings);
 	}
 
 	@Test
@@ -46,14 +85,14 @@ public class XMLFormatterSetSpaceBeforeEmptyCloseTagTest {
 		SharedSettings settings = new SharedSettings();
 		settings.getFormattingSettings().setSpaceBeforeEmptyCloseTag(true);
 
-		String content = "<a>\r" + //
-				" <b />\r" + //
+		String content = "<a>\r\n" + //
+				" <b />\r\n" + //
 				"</a>";
-		String expected = "<a>\r" + //
-				"  <b />\r" + //
+		String expected = "<a>\r\n" + //
+				"  <b />\r\n" + //
 				"</a>";
 		assertFormat(content, expected, settings, //
-				te(0, 3, 1, 1, "\r  "));
+				te(0, 3, 1, 1, "\r\n  "));
 		assertFormat(expected, expected, settings);
 	}
 
@@ -62,30 +101,32 @@ public class XMLFormatterSetSpaceBeforeEmptyCloseTagTest {
 		SharedSettings settings = new SharedSettings();
 		settings.getFormattingSettings().setSpaceBeforeEmptyCloseTag(false);
 
-		String content = "<a>\r" + //
-				" <b/>\r" + //
+		String content = "<a>\r\n" + //
+				" <b/>\r\n" + //
 				"</a>";
-		String expected = "<a>\r" + //
-				"  <b/>\r" + //
+		String expected = "<a>\r\n" + //
+				"  <b/>\r\n" + //
 				"</a>";
 		assertFormat(content, expected, settings, //
-				te(0, 3, 1, 1, "\r  "));
+				te(0, 3, 1, 1, "\r\n  "));
 		assertFormat(expected, expected, settings);
 	}
 
-	@Disabled
 	@Test
 	public void testSelfCloseTagSpaceFalseAlreadyHasSpace() throws BadLocationException {
 		SharedSettings settings = new SharedSettings();
 		settings.getFormattingSettings().setSpaceBeforeEmptyCloseTag(false);
 
-		String content = "<a>\r" + //
-				" <b />\r" + //
+		String content = "<a>\r\n" + //
+				" <b />\r\n" + //
 				"</a>";
-		String expected = "<a>\r" + //
-				"  <b/>\r" + //
+		String expected = "<a>\r\n" + //
+				"  <b/>\r\n" + //
 				"</a>";
-		assertFormat(content, expected, settings);
+		assertFormat(content, expected, settings,
+				te(0, 3, 1, 1, "\r\n  "),
+				te(1, 3, 1, 4, ""));
+		assertFormat(expected, expected, settings);
 	}
 
 	@Test
@@ -93,14 +134,14 @@ public class XMLFormatterSetSpaceBeforeEmptyCloseTagTest {
 		SharedSettings settings = new SharedSettings();
 		settings.getFormattingSettings().setSpaceBeforeEmptyCloseTag(false);
 
-		String content = "<a>\r" + //
-				" <b\r" + //
+		String content = "<a>\r\n" + //
+				" <b\r\n" + //
 				"</a>";
-		String expected = "<a>\r" + //
-				"  <b\r" + //
+		String expected = "<a>\r\n" + //
+				"  <b\r\n" + //
 				"</a>";
 		assertFormat(content, expected, settings, //
-				te(0, 3, 1, 1, "\r  "));
+				te(0, 3, 1, 1, "\r\n  "));
 		assertFormat(expected, expected, settings);
 
 	}
@@ -110,14 +151,14 @@ public class XMLFormatterSetSpaceBeforeEmptyCloseTagTest {
 		SharedSettings settings = new SharedSettings();
 		settings.getFormattingSettings().setSpaceBeforeEmptyCloseTag(false);
 
-		String content = "<a>\r" + //
-				" <b> Value </b\r" + //
+		String content = "<a>\r\n" + //
+				" <b> Value </b\r\n" + //
 				"</a>";
-		String expected = "<a>\r" + //
-				"  <b> Value </b\r" + //
+		String expected = "<a>\r\n" + //
+				"  <b> Value </b\r\n" + //
 				"</a>";
 		assertFormat(content, expected, settings, //
-				te(0, 3, 1, 1, "\r  "));
+				te(0, 3, 1, 1, "\r\n  "));
 		assertFormat(expected, expected, settings);
 	}
 
