6e6fe51918a79c10d2005a7f1313b0a234785dd7
Fred Bricon <fbricon@gmail.com>
2023-03-20 11:53:01 +0100

    Removed some unused imports
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/telemetry/DocumentTelemetryInfo.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/telemetry/DocumentTelemetryInfo.java
@@ -13,8 +13,6 @@ package org.eclipse.lemminx.telemetry;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 import org.eclipse.lemminx.dom.DOMDocument;
