c09d7f9742b8a2e2fdd860fdcc58b5081267e47f
Fred Bricon <fbricon@gmail.com>
2023-03-20 11:53:01 +0100

    Removed some unused imports
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/extensions/contentmodel/participants/diagnostics/XMLValidator.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/extensions/contentmodel/participants/diagnostics/XMLValidator.java
@@ -14,7 +14,6 @@ package org.eclipse.lemminx.extensions.contentmodel.participants.diagnostics;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
