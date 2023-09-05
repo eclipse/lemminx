2767b9987637533d87fa2d7d6a15133e244d7774
Omar Farag <omarfarag74@gmail.com>
2023-09-05 17:01:00 -0400

    Add tagname to code action
    Signed-off-by: Omar Farag <omarfarag74@gmail.com>

--- org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/extensions/contentmodel/participants/codeactions/src_annotationCodeAction.java
+++ org.eclipse.lemminx/src/main/java/org/eclipse/lemminx/extensions/contentmodel/participants/codeactions/src_annotationCodeAction.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 
 import org.eclipse.lsp4j.Range;
+import org.eclipse.lemminx.commons.BadLocationException;
 import org.eclipse.lemminx.dom.DOMDocument;
+import org.eclipse.lemminx.dom.DOMElement;
+import org.eclipse.lemminx.dom.DOMNode;
 import org.eclipse.lemminx.services.extensions.codeaction.ICodeActionParticipant;
 import org.eclipse.lemminx.services.extensions.codeaction.ICodeActionRequest;
 
@@ -20,15 +23,27 @@ public class src_annotationCodeAction implements ICodeActionParticipant {
 		Diagnostic diagnostic = request.getDiagnostic();
 		DOMDocument document = request.getDocument();
 		Range diagnosticRange = diagnostic.getRange();
+		String codeActionText;
+
+		// Attempt to get tag name
+		try {
+			int startOffset = document.offsetAt(diagnosticRange.getStart()) + 1;
+			DOMNode node = document.findNodeAt(startOffset);
+			DOMElement element = (DOMElement) node;
+			String tagName = element.getTagName();
+			codeActionText = "Replace '" + tagName + "' with ";
+		} catch (BadLocationException e) {
+			codeActionText = "Replace with ";
+		}
 		
 	
 		// Replace with "appinfo"
-		CodeAction replaceAction_appinfo = CodeActionFactory.replace("Replace with \"appinfo\"", diagnosticRange,
+		CodeAction replaceAction_appinfo = CodeActionFactory.replace(codeActionText + "'xs:appinfo'", diagnosticRange,
 				"xs:appinfo",
 				document.getTextDocument(), diagnostic);
 		
 		// Replace with "documentation"
-		CodeAction replaceAction_documentation = CodeActionFactory.replace("Replace with \"documentation\"", diagnosticRange,
+		CodeAction replaceAction_documentation = CodeActionFactory.replace(codeActionText + "'xs:documentation'", diagnosticRange,
 				"xs:documentation",
 				document.getTextDocument(), diagnostic);
 
